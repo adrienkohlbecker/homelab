23d8048ec75b0578d5381b0960ba3c549d126011
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2014-07-09 04:35:22 +0200

    Shell check

--- roles/snapraid/files/snapraid_scrub.sh
+++ roles/snapraid/files/snapraid_scrub.sh
@@ -5,10 +5,6 @@ test -e /usr/local/lib/bash-framework && source /usr/local/lib/bash-framework ||
 #        SCRIPT CONFIG         #
 ################################
 
-# Threshold of deleted files over which to show an error
-# Useful in case of mass deletion by human error / missing device
-DEL_THRESHOLD=50
-
 # Destination email
 EMAIL_TO="root"
 
@@ -36,21 +32,21 @@ br
 
 run "snapraid scrub 2>&1"
 
-if grep -q "Everything OK" $TMP_OUTPUT; then
+if grep -q "Everything OK" "$TMP_OUTPUT"; then
   log "Everything looks good"
   pushover_log "OK :: Scrub finished sucessfully" "-1"
   exit 0
-elif grep -q "WARNING! There are errors" $TMP_OUTPUT; then
+elif grep -q "WARNING! There are errors" "$TMP_OUTPUT"; then
   READ_COUNT=$(grep '[0-9]\{1,\} read errors$' "$TMP_OUTPUT" | sed 's/ \+/ /g' | cut -d ' ' -f2)
   DATA_COUNT=$(grep '[0-9]\{1,\} data errors$' "$TMP_OUTPUT" | sed 's/ \+/ /g' | cut -d ' ' -f2)
 
   log "Scrub errors summary: Read [$READ_COUNT] - Data [$DATA_COUNT]"
-  mail -s "$EMAIL_SUBJECT_PREFIX - Errors found during scrub" "$EMAIL_TO" < $TMP_OUTPUT
+  mail -s "$EMAIL_SUBJECT_PREFIX - Errors found during scrub" "$EMAIL_TO" < "$TMP_OUTPUT"
   pushover_log "ERROR :: Errors found during scrub" "1"
   exit 1
 else
   log "An unexpected error has happened."
-  mail -s "$EMAIL_SUBJECT_PREFIX ERROR - An unexpected error has happended during scrub" "$EMAIL_TO" < $TMP_OUTPUT
+  mail -s "$EMAIL_SUBJECT_PREFIX ERROR - An unexpected error has happended during scrub" "$EMAIL_TO" < "$TMP_OUTPUT"
   pushover_log "ERROR :: Unexpected error during scrub" "1"
   exit 1
 fi
