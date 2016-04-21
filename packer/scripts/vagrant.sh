6c0354159a26e00aec64ca123a8376671ad47d7e
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-04-21 18:05:37 +0200

    Cleaner box

--- packer/scripts/vagrant.sh
+++ packer/scripts/vagrant.sh
@@ -9,40 +9,17 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Update the box
 apt-get -y update >/dev/null
-apt-get -y install facter "linux-headers-$(uname -r)" build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y install facter
 apt-get -y upgrade >/dev/null
 
 # Set up sudo
 echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
-
-# Tweak sshd to prevent DNS resolution (speed up logins)
-echo 'UseDNS no' >> /etc/ssh/sshd_config
-
-# Remove 5s grub timeout to speed up booting
-cat <<EOF > /etc/default/grub
-# If you change this file, run 'update-grub' afterwards to update
-# /boot/grub/grub.cfg.
-
-GRUB_DEFAULT=0
-GRUB_TIMEOUT=0
-GRUB_DISTRIBUTOR=$(lsb_release -i -s 2> /dev/null || echo Debian)
-GRUB_CMDLINE_LINUX_DEFAULT="quiet"
-GRUB_CMDLINE_LINUX="debian-installer=en_US"
-EOF
-
-update-grub
-
-# Vagrant specific
-date > /etc/vagrant_box_build_time
 
 # Installing vagrant keys
 mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
-
-# Customize the message of the day
-echo 'Welcome to your Vagrant-built virtual machine.' > /etc/motd
 
 reboot
 sleep 60
