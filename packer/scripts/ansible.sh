a65666d4b1042a34f49a6db1a39960134f0f0252
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-04-21 03:15:52 +0200

    Fix ansible install

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -7,11 +7,6 @@ IFS=$'\n\t'
 
 export DEBIAN_FRONTEND=noninteractive
 
-# for add-apt-repository
-apt-get -y update >/dev/null
-apt-get install -y software-properties-common >/dev/null
-
 # Install ansible for provisioning
-add-apt-repository -y ppa:ansible/ansible >/dev/null
 apt-get -y update >/dev/null
 apt-get -y install ansible >/dev/null
