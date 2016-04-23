e628f6c89e2910e1b8810275efd5e0ec1c59a971
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-04-23 05:14:43 +0200

    Add box with zfs root

--- packer/scripts/volume.sh
+++ packer/scripts/volume.sh
@@ -10,34 +10,17 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update >/dev/null
 apt-get -y install zfsutils-linux parted >/dev/null
 
-# /dev/sdi is the first disk on the sata controller => the install is done there
+# /dev/sdi and /dev/sdj are the first disk on the sata controller => the install is done there
 
-# SSDs
-parted -s /dev/sda -- mklabel msdos
-parted -s /dev/sda -- mkpart primary 0% 512MB
-parted -s /dev/sda -- mkpart primary 512MB 100%
-parted -s /dev/sda -- set 1 boot on
-sleep 2
+ls -al /dev/disk/by-id
 
-parted -s /dev/sdb -- mklabel msdos
-parted -s /dev/sdb -- mkpart primary 0% 512MB
-parted -s /dev/sdb -- mkpart primary 512MB 100%
-parted -s /dev/sdb -- set 1 boot on
-sleep 2
-
-mkfs.ext4 -L boot /dev/sda1
-mkfs.ext4 -L boot2 /dev/sdb1
-
-zpool create -f -o ashift=12 -O compression=lz4 -O mountpoint=none \
-  rpool mirror /dev/sda2 /dev/sdb2
-zfs create -o mountpoint=none rpool/ROOT
-zfs create -o mountpoint=/mirror rpool/ROOT/ubuntu-1
+# ssds
 zfs create -o mountpoint=/mnt/docker rpool/docker
 zfs create -o mountpoint=/var/lib/libvirt/images rpool/vms
 
 # Tank
 zpool create -f -o ashift=12 -O compression=lz4 -O casesensitivity=insensitive -O normalization=formD -O mountpoint=none \
-  tank raidz2 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh
+  tank raidz2 /dev/sda /dev/sdb /dev/sdc /dev/sdd /dev/sde /dev/sdf
 zfs create -o mountpoint=/mnt/legacy tank/legacy
 zfs create -o mountpoint=/mnt/pictures tank/pictures
 zfs create -o mountpoint=/mnt/timemachine tank/timemachine
@@ -48,24 +31,24 @@ zpool export tank
 zpool import -d /dev/disk/by-id tank
 
 # Media
-parted -s /dev/sdj -- mklabel gpt
-parted -s /dev/sdj -- mkpart primary 0% 100%
+parted -s /dev/sdg -- mklabel gpt
+parted -s /dev/sdg -- mkpart primary 0% 100%
 sleep 2
-mkfs.ext4 -L snapraid_d1 /dev/sdj1
+mkfs.ext4 -L snapraid_d1 /dev/sdg1
+
+parted -s /dev/sdh -- mklabel gpt
+parted -s /dev/sdh -- mkpart primary 0% 100%
+sleep 2
+mkfs.ext4 -L snapraid_d2 /dev/sdh1
 
 parted -s /dev/sdk -- mklabel gpt
 parted -s /dev/sdk -- mkpart primary 0% 100%
 sleep 2
-mkfs.ext4 -L snapraid_d2 /dev/sdk1
+mkfs.ext4 -L snapraid_d3 /dev/sdk1
 
 parted -s /dev/sdl -- mklabel gpt
 parted -s /dev/sdl -- mkpart primary 0% 100%
 sleep 2
-mkfs.ext4 -L snapraid_d3 /dev/sdl1
+mkfs.ext4 -L snapraid_p1 /dev/sdl1
 
-parted -s /dev/sdm -- mklabel gpt
-parted -s /dev/sdm -- mkpart primary 0% 100%
-sleep 2
-mkfs.ext4 -L snapraid_p1 /dev/sdm1
-
-apt-get -y remove zfsutils-linux parted >/dev/null
+apt-get -y remove parted >/dev/null
