7c5c906a2b3b35a6940e4bc8096948d3be17410c
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2018-05-04 00:00:37 +0100

    Update

--- ansible/roles/docker/files/docker_clean.sh
+++ ansible/roles/docker/files/docker_clean.sh
@@ -11,23 +11,7 @@ br
 log "Docker clean started."
 br
 
-STOPPED_CONTAINERS=$(docker ps -q -f "status=exited" | xargs)
-
-if [[ $STOPPED_CONTAINERS == "" ]]; then
-  log "No containers to cleanup"
-else
-  log "Cleaning exited containers"
-  run "docker rm $STOPPED_CONTAINERS 2>&1"
-fi
-
-DANGLING_IMAGES=$(docker images -q --filter "dangling=true" | xargs)
-
-if [[ $DANGLING_IMAGES == "" ]]; then
-  log "No images to cleanup"
-else
-  log "Cleaning dangling images"
-  run "docker rmi $DANGLING_IMAGES 2>&1"
-fi
+run "docker system prune -f 2>&1"
 
 deadmansnitch "98d0602056"
 log "Done"
