a2a715e32bedaba6afc511e36b2cb4127954ba57
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2018-11-01 01:41:29 +0000

    Update

--- ansible/roles/services/files/usenet-maintenance.sh
+++ ansible/roles/services/files/usenet-maintenance.sh
@@ -3,7 +3,7 @@
 IFS=$'\n\t'
 set -euxo pipefail
 
-/usr/local/bin/sort-ini /mnt/docker/sickrage/config.ini
-/usr/local/bin/sort-ini /mnt/docker/sabnzbd/sabnzbd.ini
-/usr/local/bin/sort-ini /mnt/docker/couchpotato/settings.conf
+/usr/local/bin/sort-ini /mnt/services/sickrage/config.ini
+/usr/local/bin/sort-ini /mnt/services/sabnzbd/sabnzbd.ini
+/usr/local/bin/sort-ini /mnt/services/couchpotato/settings.conf
 find /mnt/media -maxdepth 1 -mindepth 1 -type d -not -name ".tmp" -print0 | xargs -0 chown adrien:adrien
