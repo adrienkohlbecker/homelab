8047f08d365c1001b7b5d6d75869c6bcb4b37327
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2018-12-06 11:29:24 +0000

    Update zfsroot

--- packer/scripts/zfsroot.sh
+++ packer/scripts/zfsroot.sh
@@ -6,64 +6,41 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y update
-apt-get -y install zfsutils-linux zfs-initramfs parted rsync
-modprobe zfs
+apt-get -y install zfsutils zfs-initramfs
 
-# SSDs
-parted -a optimal -s /dev/sdb -- mklabel msdos
-parted -a optimal -s /dev/sdb -- mkpart primary ext3 1MB 267MB
-parted -a optimal -s /dev/sdb -- mkpart primary zfs 267MB 100%
-parted -a optimal -s /dev/sdb -- set 1 boot on
-parted -a optimal -s /dev/sdb -- print
-sleep 2
+zpool create -f -o ashift=12 -O compression=lz4 -O mountpoint=none -O atime=off -O normalization=formD -O xattr=sa \
+  rpool mirror /dev/sdb /dev/sdc
 
-parted -a optimal -s /dev/sdc -- mklabel msdos
-parted -a optimal -s /dev/sdc -- mkpart primary ext3 1MB 267MB
-parted -a optimal -s /dev/sdc -- mkpart primary zfs 267MB 100%
-parted -a optimal -s /dev/sdc -- set 1 boot on
-parted -a optimal -s /dev/sdc -- print
-sleep 2
-
-mkfs.ext3 -L boot /dev/sdb1
-mkfs.ext3 -L boot2 /dev/sdc1
-
-zpool create -f -o ashift=12 -O compression=lz4 -O mountpoint=none \
-  rpool mirror /dev/sdb2 /dev/sdc2
+# create grub partitions (zfs leaves the first 2048 sectors free)
+apt-get -y install gdisk
+sgdisk -a1 -n2:512:2047 -t2:EF02 /dev/sdb
+sgdisk -a1 -n2:512:2047 -t2:EF02 /dev/sdc
+sgdisk --print /dev/sdb
+sgdisk --print /dev/sdc
+apt-get -y remove --auto-remove gdisk
 
 zpool export rpool
 zpool import -o altroot=/mirror -d /dev/disk/by-id rpool
 
 zfs create -o mountpoint=none rpool/ROOT
-zfs create -o mountpoint=/ rpool/ROOT/bionic
+zfs create -o mountpoint=/ -o acltype=posixacl rpool/ROOT/bionic
 
-sed -i 's|^GRUB_CMDLINE_LINUX=""|GRUB_CMDLINE_LINUX="root=ZFS=rpool/ROOT/bionic boot=zfs rpool=rpool bootfs=rpool/ROOT/bionic"|' /etc/default/grub
-
-zfs set acltype=posixacl rpool/ROOT/bionic
 rsync --one-file-system -aAXHW / /mirror/
 
-rm -rf /mirror/boot
-mkdir /mirror/boot
+mount --rbind /dev  /mirror/dev
+mount --rbind /proc /mirror/proc
+mount --rbind /sys  /mirror/sys
 
-cat <<EOF > /mirror/etc/fstab
-# /etc/fstab: static file system information.
-#
-# Use 'blkid' to print the universally unique identifier for a
-# device; this may be used with UUID= as a more robust way to name devices
-# that works even if disks are added and removed. See fstab(5).
-#
-# <file system> <mount point>   <type>  <options>       <dump>  <pass>
-LABEL=boot /boot ext3 defaults 0 0
-EOF
+chroot /mirror grub-probe /
+chroot /mirror update-initramfs -u -k all
+chroot /mirror update-grub
+chroot /mirror grub-install /dev/sdb
+chroot /mirror grub-install /dev/sdc
+chroot /mirror blkid
+chroot /mirror cat /boot/grub/grub.cfg
+# remove /dev/mapper from fstab as we'll remove that disk next
+chroot /mirror sed -i '/mapper/d' /etc/fstab
 
-mount /dev/sdb1 /mirror/boot
-rsync --one-file-system -aAXHW /boot/ /mirror/boot/
-umount /mirror/boot
-mount /dev/sdb1 /boot
-
-update-grub
-grub-install /dev/sdb
-
-blkid
-cat /boot/grub/grub.cfg
-
-apt-get -y remove parted rsync
+# umount /mirror/dev
+# umount /mirror/proc
+# umount /mirror/sys
