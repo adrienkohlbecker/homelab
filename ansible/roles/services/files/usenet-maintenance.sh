7844888f54eb172ddb395dc470c9448895941ff1
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2019-07-24 14:27:53 +0200

    Add dms to usenet

--- ansible/roles/services/files/usenet-maintenance.sh
+++ ansible/roles/services/files/usenet-maintenance.sh
@@ -10,3 +10,5 @@ set -euxo pipefail
 sed -i 's/[ \t]*$//' /mnt/services/sabnzbd/autoProcessMedia.cfg
 
 find /mnt/media -maxdepth 1 -mindepth 1 -type d -not -name ".tmp" -print0 | xargs -0 chown -R ak:ak
+
+dms 150c9a2135
