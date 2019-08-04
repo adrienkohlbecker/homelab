9f109ab3eeb91fc51c559af1c2af5384520222c4
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2019-08-04 15:47:05 +0200

    Update hard drives

--- ansible/roles/smart/files/smart_long_test.sh
+++ ansible/roles/smart/files/smart_long_test.sh
@@ -10,13 +10,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Testing schedule (index is the day, Monday=0, Sunday=6)
 declare -a SCHEDULE=(
-  "/dev/disk/by-id/ata-WDC_WD10JFCX-68N6GN0_WD-WXA1E63DJRT8 /dev/disk/by-id/ata-ST8000AS0002-1NA17Z_Z8403NE6"
-  "/dev/disk/by-id/ata-WDC_WD10JFCX-68N6GN0_WD-WXA1E63DLZR6 /dev/disk/by-id/ata-ST8000AS0002-1NA17Z_Z840AL1B"
-  "/dev/disk/by-id/ata-WDC_WD10JFCX-68N6GN0_WD-WXA1E63EVZN6 /dev/disk/by-id/ata-Hitachi_HDS5C3030BLE630_MC49215X0B7AYA"
-  "/dev/disk/by-id/ata-WDC_WD10JFCX-68N6GN0_WD-WXA1E63EXDD4 /dev/disk/by-id/ata-Hitachi_HDS5C3030BLE630_MC49215X0B7DNA"
-  "/dev/disk/by-id/ata-WDC_WD10JFCX-68N6GN0_WD-WXA1E63FXKH5 /dev/disk/by-id/ata-WDC_WD30EZRX-00D8PB0_WD-WMC4N1263858"
-  "/dev/disk/by-id/ata-WDC_WD10JFCX-68N6GN0_WD-WXN1E53SA095 /dev/disk/by-id/ata-WDC_WD30EZRX-00SPEB0_WD-WCC4E1229185"
-  "/dev/disk/by-id/ata-Crucial_CT240M500SSD1_13410953598E /dev/disk/by-id/ata-Crucial_CT240M500SSD1_14270C8904C3 /dev/disk/by-id/ata-WDC_WD20EZRX-00D8PB0_WD-WMC4M0859498"
+"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_2YKG8G2D"
+"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_JEJYH3LZ"
+"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_JEK2LK1Z"
+"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_JEKG9J2Z"
+"/dev/disk/by-id/ata-ST8000AS0002-1NA17Z_Z8403NE6"
+"/dev/disk/by-id/ata-ST8000AS0002-1NA17Z_Z840AL1B"
+"/dev/disk/by-id/ata-Crucial_CT240M500SSD1_13410953598E /dev/disk/by-id/ata-Crucial_CT240M500SSD1_14270C8904C3"
 )
 
 # Monday=0, Sunday=6
