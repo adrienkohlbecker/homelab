291e56122a61c337b02b37ece756dd344b99040a
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2019-09-02 14:36:51 +0200

    Tweak recordsize

--- packer/scripts/volume.sh
+++ packer/scripts/volume.sh
@@ -14,7 +14,7 @@ ls -al /dev/disk/by-id
 
 # ssds
 zfs create -o mountpoint=/mnt/services rpool/services
-zfs create -o mountpoint=/mnt/vms/ssd rpool/vms
+zfs create -o mountpoint=/mnt/vms/ssd -o recordsize=4k rpool/vms
 
 # data
 zpool create -f -o ashift=12 -O compression=lz4 -O casesensitivity=insensitive -O normalization=formD -O mountpoint=none -O atime=off -O xattr=sa \
