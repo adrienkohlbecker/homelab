0062e277dbc0881d52ade2e039c17bada92ccbc9
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2020-10-02 03:25:00 +0200

    Update webhook

--- ansible/roles/zero/files/webhook/server.js
+++ ansible/roles/zero/files/webhook/server.js
@@ -14,20 +14,53 @@ try {
 }
 
 const serialPort = new SerialPort(serialPath, { baudRate: 115200 })
+serialPort.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
 
 const app = express();
 app.use(express.json());
 
+function write(text) {
+  serialPort.write(text, function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+  });
+}
+
+var lastSeenPlaying = 0;
+var lastSeenStopped = 0;
+
+function playing() {
+  if (lastSeenPlaying < Date.now() - 1000) {
+    lastSeenPlaying = Date.now();
+    console.log("PLAYING");
+    write('$I5\r\n');
+  } else {
+    console.log("debouncing playing");
+  }
+}
+
+function stopped() {
+  if (lastSeenStopped < Date.now() - 1000) {
+    lastSeenStopped = Date.now();
+    console.log("STOPPED");
+    write('$I1\r\n');
+  } else {
+    console.log("debouncing stopped");
+  }
+}
+
 // Access the parse results as request.body
 app.post('/', function(request, response){
+    console.log("Webhook...");
     if (request.body && request.body.type === "transport-state") {
       if (request.body.data && request.body.data.roomName === "Office") {
         if (request.body.data.state && request.body.data.state.playbackState && request.body.data.state.playbackState === "PLAYING") {
-          console.log("PLAYING");
-          serialPort.write('$I5\r\n');
+          playing();
         } else {
-          console.log("STOPPED");
-          serialPort.write('$I1\r\n');
+          stopped();
         }
       }
     }
