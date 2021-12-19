c0c8c32727bf23a2e19c3f730adef7a351080104
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2021-12-19 17:19:40 +0100

    Update

--- ansible/roles/zfs/files/zfs_backup.sh
+++ ansible/roles/zfs/files/zfs_backup.sh
@@ -3,7 +3,9 @@
 IFS=$'\n\t'
 set -euxo pipefail
 
-/usr/local/bin/zfs-autobackup \
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+zfs-autobackup \
   --allow-empty \
   --buffer 1G \
   --clear-mountpoint \
@@ -17,3 +19,16 @@ set -euxo pipefail
   --verbose \
   --zfs-compressed \
   bak backup
+
+LAST_SNAPSHOT=$(zfs list -t snapshot -o name -s creation -r data/data | grep "@bak-" | tail -1 | cut -d@ -f2)
+RSYNC=(rsync -ah --delete --delete-excluded -e "ssh -i /home/ak/.ssh/id_rsa" --one-file-system --exclude .DS_Store --exclude "._*" --exclude .DocumentRevisions-V100 --exclude .Trashes --exclude .TemporaryItems)
+BUNK=ak@10.123.60.5
+
+"${RSYNC[@]}" "/mnt/data/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/data
+"${RSYNC[@]}" "/mnt/services/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/services
+"${RSYNC[@]}" "/mnt/vms/ssd/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/vms_ssd
+"${RSYNC[@]}" "/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/rpool
+"${RSYNC[@]}" "/mnt/arq/adrien/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/arq/adrien
+"${RSYNC[@]}" "/mnt/arq/marie/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/arq/marie
+"${RSYNC[@]}" "/mnt/brumath/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/brumath
+"${RSYNC[@]}" "/mnt/eckwersheim/.zfs/snapshot/$LAST_SNAPSHOT/" $BUNK:/volume1/Backup/eckwersheim
