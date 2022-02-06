990cc6ae1baf64a9da1c81c2197abef7bf66daf3
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2022-02-06 22:47:17 +0100

    Update

--- ansible/roles/smart/files/smart_long_test.sh
+++ ansible/roles/smart/files/smart_long_test.sh
@@ -10,13 +10,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Testing schedule (index is the day, Monday=0, Sunday=6)
 declare -a SCHEDULE=(
-"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_2YKG8G2D"
-"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_JEJYH3LZ"
-"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_JEK2LK1Z"
-"/dev/disk/by-id/ata-WDC_WD100EMAZ-00WJTA0_JEKG9J2Z"
-"/dev/disk/by-id/ata-ST8000AS0002-1NA17Z_Z8403NE6"
-"/dev/disk/by-id/ata-ST8000AS0002-1NA17Z_Z840AL1B"
-"/dev/disk/by-id/ata-Crucial_CT240M500SSD1_13410953598E /dev/disk/by-id/ata-Crucial_CT240M500SSD1_14270C8904C3"
+"/dev/disk/by-id/scsi-SATA_WDC_WD100EMAZ-00_2YKG8G2D"
+"/dev/disk/by-id/scsi-SATA_WDC_WD100EMAZ-00_JEJYH3LZ"
+"/dev/disk/by-id/scsi-SATA_WDC_WD100EMAZ-00_JEK2LK1Z"
+"/dev/disk/by-id/scsi-SATA_WDC_WD100EMAZ-00_JEKG9J2Z"
+"/dev/disk/by-id/scsi-SATA_WDC_WD101EFBX-68_VCJ3MYHP"
+"/dev/disk/by-id/scsi-SATA_WDC_WD101EFBX-68_VCJ3V79P"
+"/dev/disk/by-id/ata-Crucial_CT240M500SSD1_13410953598E /dev/disk/by-id/ata-Crucial_CT240M500SSD1_14270C8904C3 /dev/disk/by-id/scsi-SATA_CT240BX500SSD1_214732E6E8B4"
 )
 
 # Monday=0, Sunday=6
@@ -33,7 +33,8 @@ else
     if [ -h $device ]; then
       smartctl -t long $device
     else
-      echo "Skipping $device, does not exist"
+      echo "Device $device does not exist"
+      exit 1
     fi
   done
 fi
