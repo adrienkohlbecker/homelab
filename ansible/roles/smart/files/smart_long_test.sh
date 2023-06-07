e755f4c0eb0a750be174dbd42dd811809617ede4
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2023-06-07 15:46:33 +0200

    Add media unsafe

--- ansible/roles/smart/files/smart_long_test.sh
+++ ansible/roles/smart/files/smart_long_test.sh
@@ -15,7 +15,7 @@ declare -a SCHEDULE=(
 "/dev/disk/by-id/scsi-SATA_WDC_WD100EMAZ-00_JEK2LK1Z"
 "/dev/disk/by-id/scsi-SATA_WDC_WD100EMAZ-00_JEKG9J2Z"
 "/dev/disk/by-id/scsi-SATA_WDC_WD101EFBX-68_VCJ3MYHP"
-"/dev/disk/by-id/scsi-SATA_WDC_WD101EFBX-68_VCJ3V79P"
+"/dev/disk/by-id/scsi-SATA_WDC_WD101EFBX-68_VCJ3V79P /dev/disk/by-id/scsi-SATA_APPLE_HDD_ST1000_Z4YGHYCG"
 "/dev/disk/by-id/ata-Crucial_CT240M500SSD1_13410953598E /dev/disk/by-id/ata-Crucial_CT240M500SSD1_14270C8904C3 /dev/disk/by-id/scsi-SATA_CT240BX500SSD1_214732E6E8B4"
 )
 
