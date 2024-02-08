b23920c29d36fd88ffd6b0fa12ca49680ec9282e
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-02-08 19:48:47 +0100

    WIP

--- ansible/roles/virtualization/files/virsh_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-IFS=$'\n\t'
-set -euxo pipefail
-
-for domain in $(virsh list --all --name); do
-  virsh dumpxml "$domain" > "/mnt/vms/ssd/$domain.xml"
-done
