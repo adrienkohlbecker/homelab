2229ad1a2ccf715360ad0a8e9789b97bcb24f480
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-02-08 20:01:14 +0100

    Update

--- ansible/roles/services/files/usenet-maintenance.sh
+++ ansible/roles/services/files/usenet-maintenance.sh
@@ -9,6 +9,5 @@ set -euxo pipefail
 sed -i 's/[ \t]*$//' /mnt/services/sabnzbd/autoProcessMedia.cfg
 
 find /mnt/media -maxdepth 1 -mindepth 1 -type d -not -name ".tmp" -print0 | xargs -0 chown -R ak:ak
-find /mnt/media_unsafe -maxdepth 1 -mindepth 1 -type d -not -name ".tmp" -print0 | xargs -0 chown -R ak:ak
 
 dms 150c9a2135
