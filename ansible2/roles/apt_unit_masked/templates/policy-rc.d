08391b8a9723d7ce6b45c7c66bcf9b057bb94a5a
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-03-04 17:22:28 +0100

    Fix apt_unit_masked with multiple units

--- ansible2/roles/apt_unit_masked/templates/policy-rc.d
+++ ansible2/roles/apt_unit_masked/templates/policy-rc.d
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" = "{{ unit }}" ] || [ "$1.service" = "{{ unit }}" ]; then
+if {% for item in (unit if unit is not string else [unit]) %}[ "$1" = "{{ item }}" ] || [ "$1.service" = "{{ item }}" ]{% if not loop.last %} || {% endif %}{% endfor %}; then
   msg="denied call with [$*]"
   echo "policy-rc.d $msg" 1>&2
   logger --priority user.warn --tag policy-rc.d "$msg"
