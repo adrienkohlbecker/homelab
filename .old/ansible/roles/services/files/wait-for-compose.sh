aad1faa4dde5848bb6af734b4bb1a63f8bc31b3b
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-03-27 17:20:08 +0100

    WIP

--- .old/ansible/roles/services/files/wait-for-compose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-IFS=$'\n\t'
-set -euo pipefail
-
-while [ $(docker ps --format "{{.Names}}" | wc -l) -gt 0 ]; do
-  sleep 2
-done
