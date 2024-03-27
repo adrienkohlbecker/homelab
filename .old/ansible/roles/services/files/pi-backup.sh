25037b47730ced4d568f7cbb15b01563708c6f90
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-03-27 17:20:08 +0100

    WIP

--- .old/ansible/roles/services/files/pi-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-IFS=$'\n\t'
-set -euxo pipefail
-
-rsync -va -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /etc/pi_backup_rsa" --delete homelab@10.123.0.15:. /mnt/services/pi
