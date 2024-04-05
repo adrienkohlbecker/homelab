784152e222165ee8f521b9f06658f5df3fed1844
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-04-05 14:18:47 +0200

    ansible-lint

--- roles/apt_unit_masked/templates/policy-rc.d
+++ roles/apt_unit_masked/templates/policy-rc.d
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if {% for item in (unit if unit is not string else [unit]) %}[ "$1" = "{{ item }}" ] || [ "$1.service" = "{{ item }}" ]{% if not loop.last %} || {% endif %}{% endfor %}; then
+if {% for item in (apt_unit_masked_unit if apt_unit_masked_unit is not string else [apt_unit_masked_unit]) %}[ "$1" = "{{ item }}" ] || [ "$1.service" = "{{ item }}" ]{% if not loop.last %} || {% endif %}{% endfor %}; then
   msg="denied call with [$*]"
   echo "policy-rc.d $msg" 1>&2
   logger --priority user.warn --tag policy-rc.d "$msg"
