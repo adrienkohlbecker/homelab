51c4d70e190c7035cfe0b326769f1f7d09fd6be8
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-04-22 16:37:35 +0200

    Improve zfs_check_mount

--- roles/zfs_mount/files/zfs_check_mount.sh
+++ roles/zfs_mount/files/zfs_check_mount.sh
@@ -16,3 +16,9 @@ if [ "$OUTPUT" != "$DATASET filesystem yes $MOUNTPOINT off on" ]; then
   echo "Cannot ensure $DATASET is mounted correctly at $MOUNTPOINT, got $OUTPUT" >&2
   exit 1
 fi
+
+OUTPUT=$(mount | grep "on /mnt/services type zfs" | wc -l)
+if [ "$OUTPUT" != "1" ]; then
+  echo "Multiple ($OUTPUT) active mounts at $MOUNTPOINT, are two datasets active at the same time?" >&2
+  exit 1
+fi
