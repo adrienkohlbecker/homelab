8923448a4aff565d00fa6e499b1df5558e8268c9
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-04-23 00:40:46 +0200

    Nicer zfs_check_mount output

--- roles/zfs_mount/files/zfs_check_mount.sh
+++ roles/zfs_mount/files/zfs_check_mount.sh
@@ -7,18 +7,18 @@ DATASET=${1:-}
 MOUNTPOINT=${2:-}
 
 if [ "$DATASET" = "" ] || [ "$MOUNTPOINT" = "" ]; then
-  echo "Usage: zfs_check_mount DATASET MOUNTPOINT" >&2
+  echo >&2 "Usage: zfs_check_mount DATASET MOUNTPOINT"
   exit 1
 fi
 
 OUTPUT=$(zfs get -pH -o value name,type,mounted,mountpoint,readonly,canmount $DATASET | xargs echo -n | tr '\n' ' ')
 if [ "$OUTPUT" != "$DATASET filesystem yes $MOUNTPOINT off on" ]; then
-  echo "Cannot ensure $DATASET is mounted correctly at $MOUNTPOINT, got $OUTPUT" >&2
+  echo >&2 "Cannot ensure $DATASET is mounted correctly at $MOUNTPOINT, got $OUTPUT (name,type,mounted,mountpoint,readonly,canmount)"
   exit 1
 fi
 
 OUTPUT=$(mount | grep "on /mnt/services type zfs" | wc -l)
 if [ "$OUTPUT" != "1" ]; then
-  echo "Multiple ($OUTPUT) active mounts at $MOUNTPOINT, are two datasets active at the same time?" >&2
+  echo >&2 "Multiple ($OUTPUT) active mounts at $MOUNTPOINT, are two datasets active at the same time?"
   exit 1
 fi
