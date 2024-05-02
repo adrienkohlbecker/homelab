b21cf9dbb39e027535de69b84a587ce7b7dd3825
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-05-03 00:56:34 +0200

    Add podman health script

---
+++ roles/podman/files/wait_for_healthy_container.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# shellcheck source=../../bash/files/functions.sh
+source /usr/local/lib/functions.sh
+
+f_require_root
+
+CIDFILE="${1:-}"
+if [ -n "$CIDFILE" ]; then
+  f_fail "Usage: $F_SCRIPT CIDFILE"
+fi
+
+if [ ! -f "$CIDFILE" ]; then
+  f_fail "Error: CIDFILE '$CIDFILE' does not exist"
+fi
+
+sleep 1
+while [ "$(/usr/bin/podman container inspect "$(cat "$CIDFILE")" --format "{{.State.Healthcheck.Status}}")" != "healthy" ]; do
+  sleep 1
+done
