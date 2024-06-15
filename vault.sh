0b31181bdc620a556fa1234accb088370cab7a0f
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-06-15 22:15:54 +0000

    docker test improvements

--- vault.sh
+++ vault.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-# set the password with security add-generic-password -a ak -j "vault password ansible" -s homelab-vault -w
-/usr/bin/security find-generic-password -a ak -s homelab-vault -w
+if [ $(uname) = "Darwin" ]; then
+  # set the password with security add-generic-password -a ak -j "vault password ansible" -s homelab-vault -w
+  /usr/bin/security find-generic-password -a ak -s homelab-vault -w
+else
+  pass show homelab
+fi
