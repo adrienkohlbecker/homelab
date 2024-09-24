80ea1632c8b2115274a11a1df68c793501ad48bc
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2024-09-24 11:41:29 +0200

    TMPDIR has special meaning in mktemp

--- test/testrole.sh
+++ test/testrole.sh
@@ -10,31 +10,31 @@ else
   PODMAN="podman"
 fi
 
-TMPDIR=$(mktemp -d)
-trap 'rm -rf $TMPDIR' EXIT
+WORKDIR=$(mktemp -d)
+trap 'rm -rf $WORKDIR' EXIT
 
-cp -r group_vars "$TMPDIR"
-cp -r host_vars "$TMPDIR"
-cp -r wireguard "$TMPDIR"
-cp -r "roles/systemd_unit" "$TMPDIR"
-cp -r "roles/usergroup_immediate" "$TMPDIR"
-cp -r "roles/apt_unit_masked" "$TMPDIR"
-cp -r "roles/logrotate" "$TMPDIR"
-cp -r "roles/netdata" "$TMPDIR"
-cp -r "roles/nginx" "$TMPDIR"
-cp -r "roles/cron" "$TMPDIR"
-cp -r "roles/zfs_mount" "$TMPDIR"
-cp -r "roles/sort_ini" "$TMPDIR"
-cp -r "roles/_test" "$TMPDIR"
-cp -r "roles/$ROLE" "$TMPDIR"
+cp -r group_vars "$WORKDIR"
+cp -r host_vars "$WORKDIR"
+cp -r wireguard "$WORKDIR"
+cp -r "roles/systemd_unit" "$WORKDIR"
+cp -r "roles/usergroup_immediate" "$WORKDIR"
+cp -r "roles/apt_unit_masked" "$WORKDIR"
+cp -r "roles/logrotate" "$WORKDIR"
+cp -r "roles/netdata" "$WORKDIR"
+cp -r "roles/nginx" "$WORKDIR"
+cp -r "roles/cron" "$WORKDIR"
+cp -r "roles/zfs_mount" "$WORKDIR"
+cp -r "roles/sort_ini" "$WORKDIR"
+cp -r "roles/_test" "$WORKDIR"
+cp -r "roles/$ROLE" "$WORKDIR"
 
-cat <<EOF >"$TMPDIR/site.yml"
+cat <<EOF >"$WORKDIR/site.yml"
 - hosts: box
   roles:
     - $ROLE
 EOF
 if [ -f "roles/$ROLE/tasks/_test.yml" ]; then
-  cat <<EOF >>"$TMPDIR/_test.yml"
+  cat <<EOF >>"$WORKDIR/_test.yml"
 - hosts: box
   tasks:
     - import_role:
@@ -46,32 +46,32 @@ fi
 stop() {
   if [ "${KEEPAROUND:-}" = "1" ]; then
     echo "ssh -i packer/vagrant.key -p $PORT root@localhost"
-    echo "$PODMAN stop --ignore --time 5 $(cat $TMPDIR/cid)"
+    echo "$PODMAN stop --ignore --time 5 $(cat $WORKDIR/cid)"
   else
-    $PODMAN stop --ignore --time 5 --cidfile $TMPDIR/cid
+    $PODMAN stop --ignore --time 5 --cidfile $WORKDIR/cid
   fi
-  rm -rf "$TMPDIR"
+  rm -rf "$WORKDIR"
 }
 
 err() {
   TMPFILE=$(mktemp)
-  $PODMAN exec --tty "$(cat $TMPDIR/cid)" journalctl --pager-end --no-pager --priority info >$TMPFILE
+  $PODMAN exec --tty "$(cat $WORKDIR/cid)" journalctl --pager-end --no-pager --priority info >$TMPFILE
   echo "$TMPFILE"
 }
 
 trap err ERR
 trap stop EXIT
 
-$PODMAN run --interactive --rm --publish 127.0.0.1::22 --detach --privileged --cidfile $TMPDIR/cid --timeout 600 homelab
+$PODMAN run --interactive --rm --publish 127.0.0.1::22 --detach --privileged --cidfile $WORKDIR/cid --timeout 600 homelab
 
-while [ ! -f $TMPDIR/cid ]; do
+while [ ! -f $WORKDIR/cid ]; do
   echo "."
   sleep 1
 done
 
 sleep 2
 
-ADDR=$($PODMAN port $(cat $TMPDIR/cid) 22)
+ADDR=$($PODMAN port $(cat $WORKDIR/cid) 22)
 PORT="${ADDR#*:}"
 
 while [ -z "$(socat -T2 stdout tcp:127.0.0.1:$PORT,connect-timeout=2,readbytes=1 2>/dev/null)" ]; do
@@ -81,37 +81,37 @@ done
 
 set -x
 
-if [ -f "$TMPDIR/_test.yml" ]; then
-  ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/_test.yml"
+if [ -f "$WORKDIR/_test.yml" ]; then
+  ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/_test.yml"
 fi
 
 if [[ ${1:-} == "--checkmode" ]]; then
   shift
 
-  LIST_TAGS=$(ansible-playbook "$TMPDIR/site.yml" --list-tags)
+  LIST_TAGS=$(ansible-playbook "$WORKDIR/site.yml" --list-tags)
 
-  ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --check "$@"
+  ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --check "$@"
 
   if [[ $LIST_TAGS == *"_check_stage1"* ]]; then
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --tags _check_stage1 "$@"
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --check "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --tags _check_stage1 "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --check "$@"
   fi
 
   if [[ $LIST_TAGS == *"_check_stage2"* ]]; then
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --tags _check_stage2 "$@"
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --check "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --tags _check_stage2 "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --check "$@"
   fi
 
   if [[ $LIST_TAGS == *"_check_stage3"* ]]; then
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --tags _check_stage3 "$@"
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --check "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --tags _check_stage3 "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --check "$@"
   fi
 
   if [[ $LIST_TAGS == *"_check_stage4"* ]]; then
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --tags _check_stage4 "$@"
-    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" --check "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --tags _check_stage4 "$@"
+    ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" --check "$@"
   fi
 
 fi
 
-ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$TMPDIR/site.yml" "$@"
+ansible-playbook -e docker_test=true -e ansible_ssh_port=$PORT --inventory test/inventory.ini "$WORKDIR/site.yml" "$@"
