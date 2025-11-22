473ff48528430021dd78e81bbd970b110e5e3061
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2025-11-22 14:08:54 +0100

    Add user role to pyinfra

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from pyinfra.operations import apt
 
 from roles.bash import bash
+from roles.user import user
 
 bash.apply()
+user.apply()
 
 if __name__ == "__main__":
     print("uv run pyinfra inventory.py deploy.py")
