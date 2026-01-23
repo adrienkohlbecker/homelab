fc6945fdda9ab7d266797d74e9bb1eb39e6f6992
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2026-01-23 13:38:45 +0100

    Update shebang

--- test/testall.py
+++ test/testall.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run
 """
 Test runner for Ansible roles using native asyncio parallelism.
 
--- test/testrole.py
+++ test/testrole.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run
 """
 Configure and run a single role test with colored output.
 
