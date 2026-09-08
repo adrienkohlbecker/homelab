dbe8317fa38a666361d96579f07bb2a007833888
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2026-09-08 12:14:32 +0200

    AWS: Use default S3 encryption

--- terraform/aws_ci.tf
+++ terraform/aws_ci.tf
@@ -175,16 +175,6 @@ resource "aws_s3_bucket_versioning" "ci_qemu_images" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "ci_qemu_images" {
-  bucket = aws_s3_bucket.ci_qemu_images.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "ci_qemu_images" {
   bucket = aws_s3_bucket.ci_qemu_images.id
 
