8d9e7d1537e6406ed5d1e6e31fd5c155e1c3eeff
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2026-09-13 18:54:31 +0200

    CI: Add ARM qemu host bake

--- unit_tests/test_gitlab_ci.py
+++ unit_tests/test_gitlab_ci.py
@@ -26,3 +26,26 @@ def test_lab_qemu_image_is_published_for_supported_releases() -> None:
         "resource_group": "qemu_image_lab_$UBUNTU",
         "script": ['mise run packer:publish-qemu lab --ubuntu "$UBUNTU" --promote'],
     }
+
+
+def test_qemu_host_ami_uses_one_architecture_matrix_and_promotion_flow() -> None:
+    pipeline = yaml.safe_load((ROOT / ".gitlab-ci.yml").read_text())
+    job = pipeline["qemu_host_ami"]
+
+    assert job["parallel"]["matrix"] == [
+        {
+            "QEMU_HOST_ARCHITECTURE": "x86_64",
+            "AWS_REGION": "eu-central-1",
+            "MISE_DISABLE_TOOLS": "",
+        },
+        {
+            "QEMU_HOST_ARCHITECTURE": "aarch64",
+            "AWS_REGION": "eu-west-1",
+            "MISE_DISABLE_TOOLS": "aqua:Kampfkarren/selene",
+        },
+    ]
+    assert job["resource_group"] == "ami-qemu-host-$QEMU_HOST_ARCHITECTURE-noble"
+    assert job["script"][-2:] == [
+        'source mise-tasks/ci/aws-oidc.sh arn:aws:iam::000390721279:role/homelab-ci-bake "qemu-host-ami-$CI_JOB_ID" --region "$AWS_REGION"',
+        'mise run packer:qemu-host-ami --architecture "$QEMU_HOST_ARCHITECTURE" --region "$AWS_REGION" --promote',
+    ]
