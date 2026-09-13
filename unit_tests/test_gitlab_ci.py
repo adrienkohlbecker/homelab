b80f06b0a8ae20056e9c271d845918cb71d7a236
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2026-09-13 10:40:28 +0200

    CI: Pin Ansible config globally

--- unit_tests/test_gitlab_ci.py
+++ unit_tests/test_gitlab_ci.py
@@ -5,6 +5,12 @@ import yaml
 ROOT = Path(__file__).parents[1]
 
 
+def test_ansible_config_is_global() -> None:
+    pipeline = yaml.safe_load((ROOT / ".gitlab-ci.yml").read_text())
+
+    assert pipeline["variables"]["ANSIBLE_CONFIG"] == "$CI_PROJECT_DIR/ansible.cfg"
+
+
 def test_child_pipeline_forwards_pipeline_variables() -> None:
     pipeline = yaml.safe_load((ROOT / ".gitlab-ci.yml").read_text())
 
